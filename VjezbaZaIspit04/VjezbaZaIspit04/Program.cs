1e5e63b6c5f7e9c85e496b88ad6a35d2cbd79de0
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-12-04 16:32:35 +0100

    Add files via upload

---
+++ VjezbaZaIspit04/VjezbaZaIspit04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbaZaIspit04
+{
+    internal class Program
+    {
+
+        class KlasaX
+        {
+            private int broj;
+
+            public int Broj { get => broj; set => broj = value; }
+ 
+        }
+
+        static void Main(string[] args)
+        {
+            KlasaX x = new KlasaX();
+            x.Broj = 10;
+            Console.WriteLine(x.Broj);
+            Console.ReadKey();
+        }
+    }
+}
